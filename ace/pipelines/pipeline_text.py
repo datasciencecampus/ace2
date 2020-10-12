78bcffda61875295249171401328bec67b6fa7ef
Martin Wood <martinwood@C02CF7NAMD6R.local>
2020-10-12 13:51:02 +0100

    fiddling with imports

--- ace/pipelines/pipeline_text.py
+++ ace/pipelines/pipeline_text.py
@@ -29,14 +29,14 @@ import bz2
 import json
 import os
 import pickle
-from datetime import datetime
 import string
 import re
+import wordninja
+
 from collections import Counter
+from datetime import datetime
 from os import path
 
-import pandas as pd
-import wordninja
 from nltk import word_tokenize, PorterStemmer, pos_tag
 from nltk.corpus import wordnet, stopwords
 from nltk.stem import WordNetLemmatizer
@@ -44,21 +44,22 @@ from sklearn.feature_extraction.text import strip_accents_ascii
 from sklearn.base import BaseEstimator, TransformerMixin
 from spellchecker import SpellChecker
 from sklearn.pipeline import Pipeline
+from matplotlib import pyplot as plt
 
-from  matplotlib import pyplot as plt
+import pandas as pd
 
 from ace.utils.utils import create_load_balance_hist
 
 
 def configure_pipeline(data_path, experiment_path, spell=True, split_words=True, text_header='RECDESC'):
-    base_path=path.join(experiment_path, 'text')
+    base_path = path.join(experiment_path, 'text')
     config_path = path.join(base_path, 'config.json')
     d={
-        'spell':spell,
+        'spell': spell,
         'split_words': split_words,
-        'data_path':data_path,
-        'base_path':base_path,
-        'text_header':text_header
+        'data_path': data_path,
+        'base_path': base_path,
+        'text_header': text_header
     }
     if not path.exists(base_path):
         os.makedirs(base_path)
@@ -86,29 +87,31 @@ class LemmaTokenizer(object):
         text = word_tokenize(doc)
         pos_tagged_tokens = pos_tag(text)
         return [self.lemmatize_with_pos(t) for t in pos_tagged_tokens]
-    
+
+
 class StemTokenizer(object):
     def __init__(self):
         self.ps = PorterStemmer()
 
-        
         self.stop_words = stopwords.words('english')
-        #read from text file ad config
-        self.stop_words.extend(['shouldv', 'youv', 'abov', 'ani', 'becau', 'becaus', 'befor', 'doe', 'dure', 'ha', 'hi', 'onc', 'onli', 'ourselv', 'themselv', 'thi', 'veri', 'wa', 'whi', 'yourselv'])
+        # Read from text file ad config
+        self.stop_words.extend(['shouldv', 'youv', 'abov', 'ani', 'becau', 'becaus', 'befor', 'doe', 'dure', 'ha',
+                                'hi', 'onc', 'onli', 'ourselv', 'themselv', 'thi', 'veri', 'wa', 'whi', 'yourselv'])
         
     def __call__(self, doc):
-            return [self.ps.stem(t) for t in word_tokenize(doc) if t not in self.stop_words]
-    
+        return [self.ps.stem(t) for t in word_tokenize(doc) if t not in self.stop_words]
+
+
 def lowercase_strip_accents_and_ownership(doc):
     lowercase_no_accents_doc = strip_accents_ascii(str(doc).lower())
-    txt = lowercase_no_accents_doc.replace('"', '').replace("\'s", "").replace("\'ve", " have").replace("\'re",
+    return lowercase_no_accents_doc.replace('"', '').replace("\'s", "").replace("\'ve", " have").replace("\'re",
                                                                                                         " are").replace(
         "\'", "").strip("`").strip()
-    return txt
 
-def stop(tokensin, unigrams, ngrams, digits=True):
+
+def stop(tokens_in, unigrams, ngrams, digits=True):
     new_tokens = []
-    for token in tokensin:
+    for token in tokens_in:
         ngram = token.split()
         if len(ngram) == 1:
             if ngram[0] not in unigrams and not ngram[0].isdigit():
@@ -144,12 +147,6 @@ class SpellCheckDoc(BaseEstimator, TransformerMixin):
                     corrected_text.append(word.upper())
             return " ".join(corrected_text)
         
-        #df = pd.DataFrame(x)
-        #for c in df.columns:
-        #    df[c] = df[c].str.translate(str.maketrans('', '', string.punctuation))
-        #    df[c] = pd.Series([_string_correction(token) for token in df[c]], name=c)
-        #    dataout = df[c]
-        
         dataout = pd.Series(x).str.translate(str.maketrans('', '', string.punctuation))
         dataout = dataout.apply(_string_correction)
         return dataout
@@ -160,7 +157,10 @@ class SplitWords(BaseEstimator, TransformerMixin):
         self.spell_ = SpellChecker(distance=1)
         self.__output_filedir = './config'
         self.stopwords_list_ = [x.upper() for x in set(stopwords.words('english'))]
-    
+        self.__lang_filepath = None
+        self.language_model_ = None
+
+    @staticmethod
     def __keep_correctly_spelled(self, original_tokens, spell):
         """ Only keep words that are correctly spelled
         params: 
@@ -179,16 +179,17 @@ class SplitWords(BaseEstimator, TransformerMixin):
         self.__lang_filepath = path.join(self.__output_filedir, f'my_lang_{dtime}.txt.gz')
         df = pd.DataFrame({'words' :[self.__keep_correctly_spelled(token, self.spell_) for token in x]})
         word_count = dict(Counter(" ".join(df['words']).split(" "))) 
-        word_count_df = pd.DataFrame.from_dict(word_count,orient='index').reset_index()
+        word_count_df = pd.DataFrame.from_dict(word_count, orient='index').reset_index()
         word_count_df.columns= ['words', 'n_appearances']
 
-        #only keep actual words
+        # Only keep actual words
         word_count_df['wordlength'] = word_count_df['words'].str.len()
         word_count_df = word_count_df[(word_count_df['wordlength'] >=3) | (word_count_df['words'].isin(self.stopwords_list_))]
         word_count_df = word_count_df.sort_values('n_appearances',ascending=False).reset_index(drop=True)
         word_count_df['words'] = word_count_df['words'].str.lower()
         word_count_df['words'].to_csv(self.__lang_filepath,index=None, header=False,compression='gzip',encoding='utf-8')
         self.language_model_ = wordninja.LanguageModel(self.__lang_filepath)
+
         return self
         
     def transform(self, x):
@@ -207,13 +208,8 @@ class SplitWords(BaseEstimator, TransformerMixin):
             output = re.sub(r"\b(\w) (?=\w\b)", r"\1", output)
             return output
         
-        #df = pd.DataFrame(x)
-        #for c in df.columns:
-        #    df[c] = pd.Series([_join_up_words(token) for token in df[c]], name=c)
-        #    dataout = df[c]
-        
         dataout = pd.Series(x).apply(_join_up_words)
-        dataout.to_csv("testing_spelling.csv")
+
         return dataout
 
 
@@ -312,19 +308,24 @@ class PipelineText:
         plt.show()
 
 
-configure_pipeline(data_path='data/USPTO-random-1000.pkl.bz2', experiment_path=path.join('outputs','soc'))
-pt = PipelineText(config_path=path.join('outputs', 'soc','text'))
-pt.fit_transform()
+#configure_pipeline(data_path='data/USPTO-random-1000.pkl.bz2', experiment_path=path.join('outputs', 'soc'))
+#pt = PipelineText(config_path=path.join('outputs', 'soc', 'text'))
+#pt.fit_transform()
 
-# df=pd.read_excel('data/lcf.xlsx')
-# X= df[['RECDESC', 'EXPDESC', 'Price', 'Shop']]
-# y=df[['EFSCODE']]
-# object = X,y
+test_text = ["Hello and welcome to another round of Calvin-Ball!",
+             "The current latest rule is that you have to provide your own sports commentary!",
+             "We really hope this round ends soon..."]
 
-# if not path.exists('data/processed'):
-#     os.makedirs('data/processed')
-#
-# with bz2.BZ2File('data/processed/lcf.pkl.bz2', 'wb') as pickle_file:
-#     pickle.dump(X, pickle_file, protocol=4, fix_imports=False)
+print(LemmaTokenizer()(test_text[0]))
+print(StemTokenizer()(test_text[1]))
 
-print()
+X = SpellCheckDoc().fit_transform(test_text)
+
+print(X)
+
+
+
+# These are all the corpora NLTK needs to work
+# nltk_corps = ['punkt', 'stopwords', 'averaged_perceptron_tagger', 'wordnet']
+
+print("done!")
