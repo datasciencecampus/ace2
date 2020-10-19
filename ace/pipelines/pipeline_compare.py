7a7e18faf2dd2391f1428511c5bce3f0de20d120
Martin Wood <martinwood@C02CF7NAMD6R.local>
2020-10-19 14:45:34 +0100

    basic pipeline working

--- ace/pipelines/pipeline_compare.py
+++ ace/pipelines/pipeline_compare.py
@@ -1,30 +1,51 @@
-"""
- - 15/10/2020
-
-This needs to take the ml classification results of two models, for the same sets of samples, and report how the two
-systems compare to one-another.
-
-Do we also want to put single system QA/metrics stuff in here later?
-
-"""
-
 import json
-import matplotlib
-matplotlib.use('Agg')
 import numpy as np
 import pandas as pd
-import matplotlib.pylab as plt
-import seaborn as sns
 
+from os import path, makedirs
 from sklearn.metrics import classification_report
 
 
-def fake_test_data(sample_size=20, stringy=False):
+def configure_pipeline(experiment_path, ml_file_path, comparison_ml_file_path, pad_char, pad_len, matched_only):
     """
-    Temporary, for getting this module up and running
+    :param experiment_path:
+    :param ml_file_path:
+    :param comparison_ml_file_path:
+    :param pad_char:
+    :param pad_len:
+    :param matched_only:
+    :return:
+    """
+    base_path = path.join(experiment_path, 'qa')
+    config_path = path.join(base_path, 'config.json')
+
+    d = {
+
+        'ml_file_path': ml_file_path,
+        'comparison_ml_file_path': comparison_ml_file_path,
+        'pad_char': pad_char,
+        'pad_len': pad_len,
+        'matched_only': matched_only,
+        'output_dir': base_path
+    }
+
+    if not path.exists(base_path):
+        makedirs(base_path)
+
+    with open(config_path, 'w') as fp:
+        json.dump(d, fp)
+
+
+def fake_results(sample_size=20, stringy=False):
+    """
+    Should've had this in my PhD...
+
+    Create dataframes simulating two Machine Learning system's predictions including whether each sample has been
+    filtered ('matched'), according to some probability threshold.
+
     :param sample_size: Number of samples to fake
     :param stringy: Whether it should be string or int
-    :return: y1, y2, y - three 1D arrays of size sample_size
+    :return: df, other_df: pandas DataFrames containing results in same format expected from pipeline_ml
     """
     import random
 
@@ -33,20 +54,17 @@ def fake_test_data(sample_size=20, stringy=False):
                            else random.randint(0, 5)
                            for x in range(sample_size)])
 
-    y1 = one_set(sample_size, stringy)
-    y2 = one_set(sample_size, stringy)
-    y = one_set(sample_size, stringy)
+    df = pd.DataFrame({"true_label": one_set(sample_size, stringy),
+                       "prediction_labels": one_set(sample_size, stringy),
+                       "matched": [np.random.choice([0, 1], p=[0.1, 0.9]) for x in range(sample_size)]})
 
-    return y1, y2, y
+    df.index = [x for x in range(sample_size)]
 
+    other_df = df[['true_label']].copy()
+    other_df['prediction_labels'] = one_set(sample_size, stringy)
+    other_df['matched'] = [np.random.choice([0, 1], p=[0.1, 0.9]) for x in range(sample_size)]
 
-def expanded_right_pad(x, pad_len=4, pad_char="0"):
-    # Do the padding
-    padded = np.array([str(entry).ljust(pad_len, pad_char) for entry in x])
-    # Fix the fail codes
-    padded = np.where(padded == "-100", "-1", padded)
-    padded = np.where(padded == "-600", "-6", padded)
-    return padded
+    return df, other_df
 
 
 def join_results(y1, y2, y_true, on_index=False):
@@ -83,6 +101,7 @@ def by_class_compare(y1, y2, y_true, on_index=False):
     :param y1: predicted classes for series of samples from system 1
     :param y2: predicted classes for series of samples from system 2
     :param y_true: true classes of series of samples
+    :param on_index: bool, whether to join using series indexes or just assume ordered
     :return: pandas.DataFrame containing results of the comparison
     """
     df = join_results(y1, y2, y_true, on_index)
@@ -130,81 +149,109 @@ def by_class_results(y_true, y_pred):
 
     results = pd.DataFrame(report).transpose().reset_index()
     results.rename(columns={'index': 'class'}, inplace=True)
-    results = results[~results.code.isin(['accuracy', 'macro avg', 'weighted avg'])]
+
     return results
 
 
-def evaluate_ml_by_class(file_path, matched_only):
-
-
-
-# Temp scripting while I get this show on the road
-y1, y2, y_true = fake_test_data(100, stringy=True)
-
-df = by_class_compare(y1, y2, y_true)
-
-print(df)
-
-# from os import path
-# from sklearn import metrics as met
-# from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, balanced_accuracy_score, zero_one_loss, f1_score, confusion_matrix, roc_curve, auc
-#
-# from sklearn.metrics import confusion_matrix, roc_curve, auc, accuracy_score
-# from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-# from sklearn.preprocessing import label_binarize, normalize
-#
-# from scripts.qa_results import qa_results_by_class, join_compare_systems
-#
-def configure_pipeline(experiment_path, data_path, alg_type, multi=True, dirname='soc'):
-    base_path = path.join(experiment_path, 'features')
-    config_path = path.join(base_path,  'config.json')
-    pipe_path = path.join(base_path, 'pipe')
-    d={
-
-        'alg_type':alg_type,
-        'multi': multi,
-        'dirname': dirname,
-        'data_filename':data_filename,
-        'train_test_ratio':train_test_ratio,
-        'threshold': threshold,
-        'accuracy':accuracy,
-
-
-    }
-    with open(config_path, 'w') as fp: json.dump(d, fp)
-
-
 class PipelineCompare:
-    def __init__(self, config_filename):
+    def __init__(self, experiment_path):
 
-        with open(config_filename, 'w') as fp:
+        base_path = path.join(experiment_path, 'qa')
+        config_path = path.join(base_path, 'config.json')
+
+        if not path.exists(base_path):
+            makedirs(base_path)
+
+        with open(config_path, 'r') as fp:
             self.__config = json.load(fp)
 
-        self._ml_file_path = self.__config('ml_file_path')
+        # Filepath to own ML results, and if desired, to comparison ML results
+        self._ml_file_path = self.__config.get('ml_file_path', None)
+        self._comparison_ml_file_path = self.__config.get('comparison_ml_file_path', None)
 
         # If the pad options are specified, then do so.
         self._pad_char = self.__config.get("pad_char", None)
         self._pad_len = self.__config.get("pad_len", None)
 
-    def validate_class_stats(self, ml_df=None, matched_only=False):
+        self._matched_only = self.__config.get("matched_only", None)
+        self._output_dir = self.__config["output_dir"]
+
+    def expanded_right_pad(self, x):
         """
-        Creates a classification report showing the results per class of the ml coding tool
+        Right-pads to config-specified length and character, hard-coded handling of the two non-coding signals
+        expected in the SIC SOC project
+        :param x: any single item that can be reliably cast to string
+        :return: standardised string of min length _pad_len
+        """
+        return str(x).ljust(self._pad_len, self._pad_char)\
+                          .replace("-100", "-1")\
+                          .replace("-600", "-6")
+
+    def prep_ml_table(self, df):
+        """
+        Some standard cleaning up for output predictions/validation files from the ml_pipeline
+        :param df: predictions DataFrame with columns true_label, prediction_labels, and matched if intending to subset
+        to records that passed the thresholding.
+        :return: two series for true and predicted values.  Returned as series to preserve index.
+        """
+        if self._matched_only:
+            df = df[df['matched'] == 1]
+
+        if self._pad_char:
+            y_true = df['true_label'].apply(self.expanded_right_pad)
+            y_pred = df['prediction_labels'].apply(self.expanded_right_pad)
+
+        else:
+            y_true = df['true_labels']
+            y_pred = df['prediction_labels']
+
+        return y_true, y_pred
+
+    def create_qa_outputs(self, ml_df=None):
+        """
+        Creates a classification table showing the results per class of the ml coding tool, and optionally a table
+        comparing the results to that of another classification solution, if specified.
+        :param ml_df: optionally feed an appropriately labelled DataFrame to this function, if not specifies it tries
+        to load the csv specified in the config file.
+        :return: None
         """
 
         # If no df provided, assume we're loading one saved elsewhere
         if not ml_df:
             ml_df = pd.read_csv(self._ml_file_path, index_col=0)
 
-        # If only examining those that passed a threshold limit, drop the rest
-        if matched_only:
-            ml_df = ml_df[ml_df['matched'] == 1]
+        y_true, y_pred = self.prep_ml_table(ml_df)
 
-        if self._pad_char & self._pad_len:
-            y_true = expanded_right_pad(ml_df['true_label'], pad_char=self._pad_char, pad_len=self._pad_len)
-            y_pred = expanded_right_pad(ml_df['prediction_labels'], pad_char=self._pad_char, pad_len=self._pad_len)
+        class_df = by_class_results(y_true, y_pred)
 
-        else:
-            y_true = ml_df['true_labels']
-            y_pred = ml_df['prediction_labels']
+        class_df.to_csv(path.join(self._output_dir, "by_class_results.csv"))
 
-        return by_class_results(y_true, y_pred)
+        if self._comparison_ml_file_path:
+            other_df = pd.read_csv(self._comparison_ml_file_path)
+            y_true_other, y_pred_other = self.prep_ml_table(other_df)
+            compare_df = by_class_compare(y_pred, y_pred_other, y_true, on_index=True)
+
+            compare_df.to_csv(path.join(self._output_dir, "ml_system_comparison.csv"))
+
+        return None
+
+
+# Temp scripting while I get this show on the road
+df, other_df = fake_results(200, stringy=True)
+
+print(df)
+print(other_df)
+
+df.to_csv("experiment/ml1.csv", index=True)
+other_df.to_csv("experiment/ml2.csv", index=True)
+
+configure_pipeline(experiment_path="experiment",
+                   ml_file_path="experiment/ml1.csv",
+                   comparison_ml_file_path="experiment/ml2.csv",
+                   pad_char="0",
+                   pad_len=4,
+                   matched_only=False)
+
+PC = PipelineCompare("experiment")
+
+PC.create_qa_outputs()
