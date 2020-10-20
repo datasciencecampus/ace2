2be58626ec0b21042c9f50830aca9c28716bbf8b
Martin Wood <martinwood@C02CF7NAMD6R.local>
2020-10-20 16:32:36 +0100

    wip:  Class should work, no tests yet, unclear how it would be used

--- ace/utils/data_balancing.py
+++ ace/utils/data_balancing.py
@@ -1,30 +1,48 @@
-import os
 import numpy as np
+import pandas as pd
 
 
 class DataBalancer:
     """
-    Handles selection of specific class subsets and/or rebalancing of asymmetric datasets.
+    Handles rebalancing of dataset through subsampling
     """
     def __init__(self,
-                 class_list=None,
                  min_class_support=20,
                  max_class_support=None,
-                 balanced=False):
+                 balanced=False,
+                 random_state=42):
 
-        self._class_list = class_list
+        self._class_list = None
         self._min_class_support = min_class_support
         self._max_class_support = max_class_support
         self._balanced = balanced
+        self._random_state = random_state
 
-    def subset_to_classes(self, X, y):
-        # TODO write test that ensures this works with y as pandas Series, np array or list,
-        # TODO and with X as pandas Series or DataFrame, ND np array or list
-        mask = [entry in self._class_list for entry in y]
-        return X[mask], y[mask]
+    def fit(self, X=None, y):
+        # Determine number from each class to be sampled
+        dynamic_limit = int(len(y) / len(pd.unique(y)))
+        self._min_class_support = np.max([dynamic_limit, self._min_class_support])
 
-    def fit(self, X, y):
+        # Identify the classes that are populous enough in the fitted data to be included
+        class_counts = pd.Series(y).value_counts()
+        self._class_list = list(class_counts[class_counts > (2 * self._min_class_support)])
+
         return None
 
     def transform(self, X, y):
-        return None
+        # If it's already a series, nothing will change.  This ensures there's an index
+        # to use for sample selection
+        ys = pd.Series(y)
+
+        # Filter to classes that have 2 x minimum required support
+        ys = ys[ys.isin(self._class_list)]
+
+        # Get the indices of (randomly selected) sub-sample
+        selection_index = ys.groupby(ys) \
+            .sample(self._min_class_support, random_state=self._random_state) \
+            .index
+
+        # Convert to boolean mask so it can be used with other data
+        sample_mask = pd.Series(y).index.isin(selection_index)
+
+        return X[sample_mask], y[sample_mask]
